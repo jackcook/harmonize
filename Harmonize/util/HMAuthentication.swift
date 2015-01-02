2e073527337a6a90a58efdb8350a434a7c4311bd
Jack Cook <jack@jackcook.nyc>
2015-01-02 13:09:58 -0500

    Integrate SoundCloud authentication and streaming

--- Harmonize/util/HMAuthentication.swift
+++ Harmonize/util/HMAuthentication.swift
@@ -6,10 +6,14 @@
 //  Copyright (c) 2015 Jack Cook. All rights reserved.
 //
 
+import AVFoundation
+
 func authenticateWithURL(url: NSURL) -> Bool {
     switch url.host! {
     case "spotify":
         return authenticateSpotify(url)
+    case "soundcloud":
+        return authenticateSoundCloud(url.query!.componentsSeparatedByString("=")[1])
     default:
         println("Error authenticating service: \(url.host!)")
         return false
@@ -40,7 +44,38 @@ func authenticateSpotify(url: NSURL) -> Bool {
 let soundCloudClientID = "baf7155dfc93f3df6428d89a64bf5a75"
 let soundCloudClientSecret = "3cd41889ed08ab4eda39156c21ee539f"
 let soundCloudCallbackURL = "harmonize-login://soundcloud"
+var soundCloudOAuthToken = ""
 
-func authenticateSoundCloud() {
+func authenticateSoundCloud(code: String) -> Bool {
+    let authURL = NSURL(string: "https://api.soundcloud.com/oauth2/token")
+    let postString = "client_id=\(soundCloudClientID)&client_secret=\(soundCloudClientSecret)&grant_type=authorization_code&redirect_uri=\(soundCloudCallbackURL)&code=\(code)"
+    let postData = postString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
     
+    let request = NSMutableURLRequest(URL: authURL!)
+    request.HTTPMethod = "POST"
+    request.setValue("\(postData!.length)", forHTTPHeaderField: "Content-Length")
+    request.setValue("multipart/form-data", forHTTPHeaderField: "Content-Type")
+    request.HTTPBody = postData
+    
+    let returnData = NSURLConnection.sendSynchronousRequest(request, returningResponse: nil, error: nil)!
+    
+    if let resultJSON = NSJSONSerialization.JSONObjectWithData(returnData, options: .MutableContainers, error: nil) as? NSDictionary {
+        if let accessToken = resultJSON["access_token"] as? String {
+            soundCloudOAuthToken = accessToken
+            downloadTrackData(70381879)
+        }
+    }
+    
+    return false
+}
+
+var player: AVAudioPlayer!
+
+func downloadTrackData(trackID: Int) {
+    let contentURL = NSURL(string: "https://api.soundcloud.com/tracks/\(trackID)/stream?client_id=\(soundCloudClientID)")!
+    let data = NSData(contentsOfURL: contentURL)!
+    
+    player = AVAudioPlayer(data: data, error: nil)
+    player.prepareToPlay()
+    player.play()
 }
