de116379d024f354b63b2d967cb8f0457c15dcac
Jack Cook <jack@jackcook.nyc>
2015-01-02 00:15:52 -0500

    Create base stuff

---
+++ Harmonize/Common.swift
@@ -0,0 +1,9 @@
+//
+//  Common.swift
+//  Harmonize
+//
+//  Created by Jack Cook on 1/2/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+import Foundation
---
+++ Harmonize/views/HarmonizeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HarmonizeViewController.swift
+//  Harmonize
+//
+//  Created by Jack Cook on 1/2/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+import UIKit
+
+class HarmonizeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
