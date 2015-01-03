8149c7a5b338a67575e3b7fb2e190f825ee9892f
Jack Cook <jack@jackcook.us>
2015-01-03 13:09:40 -0500

    Re-implemented loading view, removed HarmonizeViewController

--- Harmonize/views/LoadingViewController.swift
+++ Harmonize/views/LoadingViewController.swift
@@ -1,26 +1,20 @@
 //
-//  LoadingView.swift
+//  LoadingViewController.swift
 //  Harmonize
 //
-//  Created by Jack Cook on 1/2/15.
+//  Created by Jack Cook on 1/3/15.
 //  Copyright (c) 2015 Jack Cook. All rights reserved.
 //
 
-class LoadingView: UIView {
+class LoadingViewController: UIViewController {
     
-    var background: UIImageView!
     var gifView: FLAnimatedImageView!
     
-    var viewController: HarmonizeViewController!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
-    override init() {
-        super.init(frame: UIScreen.mainScreen().bounds)
-        
-        self.backgroundColor = UIColor(red: (37 / 255), green: (37 / 255), blue: (37 / 255), alpha: 1)
-        
-        background = UIImageView(frame: self.bounds)
-        background.image = UIImage(named: "background01.png")
-        
+    override func viewDidAppear(animated: Bool) {
         let imagePath = NSBundle.mainBundle().URLForResource("loading", withExtension: "gif")
         let stringPath = imagePath?.absoluteString
         let data = NSData(contentsOfURL: NSURL(string: stringPath!)!)
@@ -35,18 +29,17 @@ class LoadingView: UIView {
         
         gifView.frame = CGRectMake(95, (deviceSize.height - height) / 2.5, width, height)
         
-        self.addSubview(background)
-        self.addSubview(gifView)
+        self.view.addSubview(gifView)
         
         let timer = NSTimer.scheduledTimerWithTimeInterval(4.5, target: self, selector: "finished", userInfo: nil, repeats: false)
-    }
-
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     func finished() {
         gifView.image = UIImage(named: "loading.gif")
-        viewController.loadingDone()
+        self.performSegueWithIdentifier("browseSegue", sender: self)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 }
