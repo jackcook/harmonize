20ad541290e28d86bcf140c36693596d13e64d17
Jack Cook <jack@jackcook.nyc>
2015-01-22 18:40:48 -0500

    Create browse view controller

---
+++ Harmonize/views/BrowseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BrowseViewController.swift
+//  Harmonize
+//
+//  Created by Jack Cook on 1/7/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+class BrowseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
