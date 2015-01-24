d5874e13f61e6865eda29113f994e990052d1102
Jack Cook <jack@jackcook.nyc>
2015-01-24 07:14:52 -0800

    Remove Spotify and Rdio frameworks from git

---
+++ install.sh
@@ -0,0 +1,12 @@
+mkdir Frameworks
+cd Frameworks
+wget "https://github.com/spotify/ios-sdk/archive/master.zip"
+unzip master.zip
+rm master.zip
+mv ios-sdk-master/Spotify.framework Spotify.framework
+rm -rf ios-sdk-master
+wget "http://www.rdio.com/media/static/developer/ios/rdio-ios.tar.gz"
+tar -xzvf rdio-ios.tar.gz
+rm rdio-ios.tar.gz
+mv rdio-ios-*/Rdio.framework Rdio.framework
+rm -rf rdio-ios*
